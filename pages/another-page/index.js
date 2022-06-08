7aeabfd6e95d4615bad4d3a3662ab7e5e6827c6e
nald-dev <rynvva@gmail.com>
2022-06-08 08:19:34 +0700

    add another page and install twillio analytics

---
+++ pages/another-page/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AnotherPage = () => {
+    return (
+        <div
+            style={{
+                alignItems: 'center',
+                display: 'flex',
+                height: '100vh',
+                justifyContent: 'center',
+                width: '100vw'
+            }}
+        >
+            Another Page
+        </div>
+    )
+}
+
+export default AnotherPage
